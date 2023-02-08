215e9e131a0feec2a43de1f0501baf90297b85c4
Sc0rp1on13 <113503829+Sc0rp1on13@users.noreply.github.com>
2023-02-08 23:31:07 +0300

    Add files via upload

---
+++ Editor/Editor/Windows/FileOperationsWindow.cs
@@ -0,0 +1,29 @@
+﻿namespace Editor.Windows
+{
+    public class FileOperationsWindow : Window
+    {
+        public enum WindowMode
+        {
+            SAVE_FILE,
+            OPEN_FILE
+        }
+
+        public WindowMode mode = WindowMode.OPEN_FILE;
+
+        public override void Show()
+        {
+            if(mode == WindowMode.SAVE_FILE) {
+                Console.WriteLine("Введите путь, по которому хотите сохранить файл с описанием прямоугольников (ничего не вводите и нажмите Enter чтобы выйти):");
+                Serializer.SerializeAndSaveRectangles(Console.ReadLine(), Program.RectanglesWindow.Rectangles); 
+            } else {
+                Console.WriteLine("Введите путь, по которому хотите открыть файл с описанием прямоугольников (ничего не вводите и нажмите Enter чтобы выйти):"); 
+                List<Rectangle> rectangles = Serializer.LoadAndDeserializeRectangles(Console.ReadLine());
+                Program.RectanglesWindow.Rectangles.Clear();
+                Program.RectanglesWindow.Rectangles.AddRange(rectangles);
+            }
+
+            Console.Clear();
+            Program.currentWindow = Program.MainWindow;
+        }
+    }
+}
